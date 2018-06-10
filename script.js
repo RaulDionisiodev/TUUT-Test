283833b0320ce9687690ae8a2d4ea5e0490a3677
Raul Dionisio <rauldionisiosh@gmail.com>
2018-06-10 18:28:16 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// SideNav Button Initialization
+$(".button-collapse").sideNav();
+// SideNav Scrollbar Initialization
+var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+Ps.initialize(sideNavScrollbar);
+
+
+$('.collapse').collapse()
+
+// MDB Lightbox Init
+$(function () {
+    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
